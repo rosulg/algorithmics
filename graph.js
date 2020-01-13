140fe2373da943b76594cc468d231936a759e190
Robin Sulg <robinsulg@live.com>
2020-01-13 22:14:26 +0200

    format

--- graph.js
+++ graph.js
@@ -10,7 +10,7 @@ function drawGraph(G, scores) {
             'stroke-width': 1
         },
         nodeStyle: {
-            fill: function(d) {
+            fill: function (d) {
                 return colorOfNode(d.node, scores);
             }
         },
@@ -37,7 +37,7 @@ async function drawAndCalculate(n, p, directed) {
             score += closenessCentrality.get([node, n]) || 0;
         });
 
-        return { score, node };
+        return {score, node};
     });
 
     // Sort scores descending
@@ -60,21 +60,21 @@ function generate() {
 // ----- Helpers ------
 
 //Function to convert hex format to a rgb color
-function rgb2hex(rgb){
+function rgb2hex(rgb) {
     rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
     return (rgb && rgb.length === 4) ? "#" +
-        ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
-        ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
-        ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+        ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+        ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+        ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
 }
 
 // Draw the color palette on sidebar
 function paintColorPalette() {
-     const html = this.scores.slice(0, 10).map(score => {
+    const html = this.scores.slice(0, 10).map(score => {
         const color = colorOfNode(score.node, this.scores);
         return `<div class="color-palette" style="background-color: ${color}"><strong>${score.node}</strong></div>`;
     });
-     document.getElementById('palette').innerHTML = html.join('');
+    document.getElementById('palette').innerHTML = html.join('');
 }
 
 function colorOfNode(node, scores) {
